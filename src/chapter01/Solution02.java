2319e90f6377cdb6eafd46707390d2d27ae4ce63
apenin <alexzander.88@mail.ru>
2026-06-13 15:35:22 +0300

    Solution02 of Chapter01

---
+++ src/chapter01/Solution02.java
@@ -0,0 +1,17 @@
+package chapter01;
+
+import java.util.Scanner;
+
+public class Solution02 {
+    public static void main(String[] args) {
+        var in = new Scanner(System.in);
+        System.out.print("Enter an integer angle: ");
+        int angle = in.nextInt();
+
+        int normalizedWithOperator = ((angle % 360) + 360) % 360;
+        System.out.println("Normalized with the % operator: " + normalizedWithOperator);
+
+        int normalizedWithFloorMod = Math.floorMod(angle, 360);
+        System.out.println("Normalized with floorMod: " + normalizedWithFloorMod);
+    }
+}
