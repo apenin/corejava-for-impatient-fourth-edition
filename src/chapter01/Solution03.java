9bbe080ca80007837897de1095557f9ecd949f54
apenin <alexzander.88@mail.ru>
2026-06-13 17:07:56 +0300

    Solution03 of Chapter01

---
+++ src/chapter01/Solution03.java
@@ -0,0 +1,19 @@
+package chapter01;
+
+import java.util.Scanner;
+
+public class Solution03 {
+    public static void main(String[] args) {
+        var in = new Scanner(System.in);
+        System.out.print("Enter three integers: ");
+        int x = in.nextInt();
+        int y = in.nextInt();
+        int z = in.nextInt();
+
+        int maxWithConditionalOperator = (x > y) ? ((x > z) ? x : z) : ((y > z) ? y : z);
+        int maxWithMathMax = Math.max(x, Math.max(y, z));
+
+        System.out.println("Max with conditional operator: " + maxWithConditionalOperator);
+        System.out.println("Max with Math.max: " + maxWithMathMax);
+    }
+}
