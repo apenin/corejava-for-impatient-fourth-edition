e8c1bce7491c5d94c9d44f50a6b1f5c19ef5352e
apenin <alexzander.88@mail.ru>
2026-06-17 21:36:34 +0300

    Solution06 of Chapter01

---
+++ src/chapter01/Solution06.java
@@ -0,0 +1,19 @@
+package chapter01;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Solution06 {
+    public static void main(String[] args) {
+        var in = new Scanner(System.in);
+        System.out.print("Enter a positive integer number: ");
+        int number = in.nextInt();
+
+        BigInteger factorial = BigInteger.ONE;
+        for (int i = 1; i <= number; i++) {
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+
+        System.out.println("Factorial of " + number + " equals: " + factorial);
+    }
+}
