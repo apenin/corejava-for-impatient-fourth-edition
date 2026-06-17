f6a00ee37f0ec2b01959a7864df8e15e69cc7322
apenin <alexzander.88@mail.ru>
2026-06-17 21:15:42 +0300

    Solution04 of Chapter01

---
+++ src/chapter01/Solution04.java
@@ -0,0 +1,12 @@
+package chapter01;
+
+public class Solution04 {
+    public static void main(String[] args) {
+        System.out.println("Double.MIN_VALUE: " + Double.MIN_VALUE);
+        System.out.println("Math.nextUp(0.0): " + Math.nextUp(0.0));
+        System.out.println("Double.MIN_VALUE == Math.nextUp(0.0)? " + (Double.MIN_VALUE == Math.nextUp(0.0)));
+
+        System.out.println("Double.MAX_VALUE: " + Double.MAX_VALUE);
+        System.out.println("Next after Double.MAX_VALUE: " + Math.nextUp(Double.MAX_VALUE));
+    }
+}
