adab44ad353e4ef7e16afdc521d3a01c65db8fc5
apenin <alexzander.88@mail.ru>
2026-06-18 16:45:55 +0300

    Solution08 of Chapter01

---
+++ src/chapter01/Solution08.java
@@ -0,0 +1,19 @@
+package chapter01;
+
+import java.util.Scanner;
+
+public class Solution08 {
+    public static void main(String[] args) {
+        var in = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = in.nextLine();
+
+        System.out.println("Nonempty substrings:");
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                String subStr = str.substring(i, j);
+                System.out.println(subStr);
+            }
+        }
+    }
+}
