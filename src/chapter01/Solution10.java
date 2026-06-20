368760b76ba0b4af685ab739b8a11fa4c1669131
apenin <alexzander.88@mail.ru>
2026-06-20 18:56:41 +0300

    Solution10 of Chapter01

---
+++ src/chapter01/Solution10.java
@@ -0,0 +1,12 @@
+package chapter01;
+
+import java.util.random.RandomGenerator;
+
+public class Solution10 {
+    public static void main(String[] args) {
+        RandomGenerator generator = RandomGenerator.getDefault();
+        long randomLong = generator.nextLong();
+        String base36 = Long.toUnsignedString(randomLong, 36);
+        System.out.println("Random string: " + base36);
+    }
+}
