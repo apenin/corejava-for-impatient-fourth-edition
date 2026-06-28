0927e069ea61daa541fe1d8a49aa0540bba238df
apenin <alexzander.88@mail.ru>
2026-06-28 12:03:05 +0300

    Solution21 of Chapter01

---
+++ src/chapter01/Solution21.java
@@ -0,0 +1,14 @@
+package chapter01;
+
+public class Solution21 {
+    public static void main(String[] args) {
+        double avg = average(3, 4.5, -5, 0);
+        System.out.println(avg);
+    }
+
+    public static double average(double first, double... rest) {
+        double sum = first;
+        for (double v : rest) sum += v;
+        return sum / (1 + rest.length);
+    }
+}
