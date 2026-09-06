0bc852f5b6eec8aad131cf7975d10989abdd0efd
apenin <alexzander.88@mail.ru>
2026-09-06 17:15:08 +0300

    Solution05 of Chapter03

---
+++ src/chapter3/solution05/IntSequence.java
@@ -0,0 +1,13 @@
+package chapter3.solution05;
+
+public interface IntSequence {
+    default boolean hasNext() {
+        return true;
+    }
+
+    int next();
+
+    static IntSequence constant(int value) {
+        return () -> value;
+    }
+}
---
+++ src/chapter3/solution05/IntSequenceConstantMethodDemo.java
@@ -0,0 +1,11 @@
+package chapter3.solution05;
+
+public class IntSequenceConstantMethodDemo {
+    public static void main(String[] args) {
+        IntSequence sequence = IntSequence.constant(1);
+
+        while (sequence.hasNext()) {
+            System.out.println(sequence.next());
+        }
+    }
+}
