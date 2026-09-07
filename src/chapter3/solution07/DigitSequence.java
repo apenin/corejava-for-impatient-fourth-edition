74ea9da9f2e118c84f2e24337dfb5a82a9880360
apenin <alexzander.88@mail.ru>
2026-09-07 18:32:44 +0300

    Solution07 of Chapter03

---
+++ src/chapter3/solution07/DigitSequence.java
@@ -0,0 +1,27 @@
+package chapter3.solution07;
+
+import java.util.Iterator;
+
+public class DigitSequence implements Iterator<Integer> {
+    private int number;
+
+    public DigitSequence(int n) {
+        number = n;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return number != 0;
+    }
+
+    @Override
+    public Integer next() {
+        int result = number % 10;
+        number /= 10;
+        return result;
+    }
+
+    @Override
+    public void remove() {
+    }
+}
---
+++ src/chapter3/solution07/DigitSequenceDemo.java
@@ -0,0 +1,10 @@
+package chapter3.solution07;
+
+public class DigitSequenceDemo {
+    public static void main(String[] args) {
+        DigitSequence sequence = new DigitSequence(1234);
+        while (sequence.hasNext()) {
+            System.out.println(sequence.next());
+        }
+    }
+}
