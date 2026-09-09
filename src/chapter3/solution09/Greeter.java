f5016773a680ee0831c2668c762a9e091bcebca6
apenin <alexzander.88@mail.ru>
2026-09-09 17:49:08 +0300

    Solution09 of Chapter03

---
+++ src/chapter3/solution09/Greeter.java
@@ -0,0 +1,18 @@
+package chapter3.solution09;
+
+public class Greeter implements Runnable {
+    private final String target;
+    private final int n;
+
+    public Greeter(String target, int n) {
+        this.target = target;
+        this.n = n;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Hello, %s%n", target);
+        }
+    }
+}
---
+++ src/chapter3/solution09/GreeterDemo.java
@@ -0,0 +1,8 @@
+package chapter3.solution09;
+
+public class GreeterDemo {
+    public static void main(String[] args) {
+        new Thread(new Greeter("Alex", 50)).start();
+        new Thread(new Greeter("Anna", 30)).start();
+    }
+}
