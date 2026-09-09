b93f6f03a70a5a6000c745a6ac61171a57431e9d
apenin <alexzander.88@mail.ru>
2026-09-09 17:36:55 +0300

    Solution08 of Chapter03

---
+++ src/chapter3/solution08/SortUtils.java
@@ -0,0 +1,39 @@
+package chapter3.solution08;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class SortUtils {
+    public static void luckySort(ArrayList<String> strings, Comparator<String> comp) {
+        while (true) {
+            boolean correctOrder = true;
+
+            for (int i = 0; i < strings.size() - 1; i++) {
+                if (comp.compare(strings.get(i), strings.get(i + 1)) > 0) {
+                    correctOrder = false;
+                    Collections.shuffle(strings);
+                    break;
+                }
+            }
+
+            if (correctOrder) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] strings = { "bb", "a", "dddd", "ccc", "ffffff", "eeeee" };
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(strings));
+
+        System.out.println("Before lucky sorting by length: " + list);
+        luckySort(list, Comparator.comparingInt(String::length));
+        System.out.println("After lucky sorting by length: " + list);
+
+        System.out.println();
+
+        System.out.println("Before lucky sorting by reversed order: " + list);
+        luckySort(list, Comparator.reverseOrder());
+        System.out.println("After lucky sorting by reversed order: " + list);
+    }
+}
