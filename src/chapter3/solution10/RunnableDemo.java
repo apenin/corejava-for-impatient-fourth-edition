9430e5ab55ff45295e92fafddb95695c91765288
apenin <alexzander.88@mail.ru>
2026-09-11 12:41:38 +0300

    Solution10 of Chapter3

---
+++ src/chapter3/solution10/RunnableDemo.java
@@ -0,0 +1,30 @@
+package chapter3.solution10;
+
+public class RunnableDemo {
+    public static void runTogether(Runnable... tasks) {
+        for (Runnable task : tasks) {
+            new Thread(task).start();
+        }
+    }
+
+    public static void runInOrder(Runnable... tasks) {
+        for (Runnable task : tasks) {
+            task.run();
+        }
+    }
+
+    public static void main(String[] args) {
+        Runnable task1 = () -> System.out.println("111");
+        Runnable task2 = () -> System.out.println("222");
+        Runnable task3 = () -> System.out.println("333");
+        Runnable[] tasks1 = { task1, task2, task3 };
+
+        Runnable task4 = () -> System.out.println("444");
+        Runnable task5 = () -> System.out.println("555");
+        Runnable task6 = () -> System.out.println("666");
+        Runnable[] tasks2 = { task4, task5, task6 };
+
+        runTogether(tasks1);
+        runInOrder(tasks2);
+    }
+}
